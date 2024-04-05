0eb16c88b00d4b276aa34b9f505865be1d50ec79
Felipe Fazolin <felipefazolin@hotmail.com>
2024-04-05 09:08:37 -0300

    add post

--- src/app/components/DivComponent.js
+++ src/app/components/DivComponent.js
@@ -1,3 +1,15 @@
 export default function DivComponent() {
-  return <div>Esse é um component div que você pode chamar onde quiser</div>;
+  return (
+    <div className="post">
+      <img
+        className="post-img"
+        src="futuristic_fantasy_city.jpg"
+        alt="Descrição da imagem"
+      />
+      <div>
+        Esse é um component div que você pode chamar onde quiser. Aplica o grid
+        na class post
+      </div>
+    </div>
+  );
 }
